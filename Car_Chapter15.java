eccb05a80cf5e92ed2375ed8f86822080982e43b
FJG-KEI <kei.gds.pmqhk@gmail.com>
2025-08-03 22:29:55 +0900

    kadai_015を修正しました

--- Car_Chapter15.java
+++ Car_Chapter15.java
@@ -11,7 +11,12 @@ public class Car_Chapter15 {
 	}
 	
 	public void changeGear(int afterGear) {
-		this.speed = switch(afterGear) {
+		
+		System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
+		
+		this.gear = afterGear;
+		
+		this.speed = switch(this.gear) {
 		  case 1 -> 10;
 		  case 2 -> 20;
 		  case 3 -> 30;
@@ -22,7 +27,7 @@ public class Car_Chapter15 {
 	}
 	
 	public void run() {
-		System.out.println("時速" + speed + "km");
+		System.out.println("速度は時速" + this.speed + "kmです");
 	}
 
 }
