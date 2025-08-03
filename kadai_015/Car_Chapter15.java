c9bd2bfda3534ee7fe86f1b2ca4709fc1888e63d
FJG-KEI <kei.gds.pmqhk@gmail.com>
2025-08-03 21:41:14 +0900

    Add files via upload
    kadai_015を追加しました。

---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,28 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+	
+	private int gear;
+	private int speed;
+	
+	public Car_Chapter15() {
+		this.gear = 1;
+		this.speed = 10;
+	}
+	
+	public void changeGear(int afterGear) {
+		this.speed = switch(afterGear) {
+		  case 1 -> 10;
+		  case 2 -> 20;
+		  case 3 -> 30;
+		  case 4 -> 40;
+		  case 5 -> 50;
+		  default -> 10;
+		};
+	}
+	
+	public void run() {
+		System.out.println("時速" + speed + "km");
+	}
+
+}
