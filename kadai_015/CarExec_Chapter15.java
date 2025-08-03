0419642b9dee6f4e548fbf1da2e878e632822b59
FJG-KEI <kei.gds.pmqhk@gmail.com>
2025-08-03 23:38:47 +0900

    ファイルを移動しました

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,14 @@
+package kadai;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		
+		Car_Chapter15 car = new Car_Chapter15();
+		
+		car.changeGear(3);
+		
+		car.run();
+	}
+
+}
